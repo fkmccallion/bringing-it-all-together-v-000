efb67e6ae712217a6acac31fc52bff9d41457bd2
Frank McCallion <fkmccallion@gmail.com>
2018-03-27 02:48:48 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -45,9 +45,9 @@ class Dog
     DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 
-  def self.create(name:, breed:)
-    dog_hash {:name => name, :breed => breed}
-    dog = Dog.new(dog_hash) 
+  def self.create(name, breed)
+    dog_hash = {:name => name, :breed => breed}
+    dog = Dog.new(dog_hash)
     dog.save
     dog
   end
